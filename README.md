da2e3cd7663d3245b6e03679530f997f0970f553
Thomas Raehalme <thomas.raehalme@codecenter.fi>
2015-04-02 14:11:19 +0300

    Initial import.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a simple Hello World project using Spring Boot.
+
+Start the application with ``mvn spring-boot:run`` and open 
+[http://localhost:8080](http://localhost:8080) in your browser.
---
+++ src/main/java/demo/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * $Id$
+ * Copyright (c) Codecenter Oy. All rights reserved.
+ *
+ * This software is the proprietary information of Codecenter Oy.
+ * Use is subject to license terms.
+ */
+package demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World";
+    }
+}
